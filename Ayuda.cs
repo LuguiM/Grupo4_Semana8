4b3baf9b5913144c83337e346d9d5e44624df4d7
Estela25 <USIS035821@ugb.edu.sv>
2021-09-17 20:29:52 -0600

    form de ayuda

---
+++ Ayuda.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Semana8_Grupo4
+{
+    public partial class Ayuda : Form
+    {
+        public Ayuda()
+        {
+            InitializeComponent();
+        }
+    }
+}
